ab5a5c0c287c2fec3a36ff360981d77bd22e76f8
Yashvardhan Singh <draco882003@gmail.com>
2025-04-05 21:53:15 +0530

    Add plot configurations

---
+++ utils/plot_config.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+
+def init_styles() -> None:
+    """Sets the default `matplotlib` plot styles."""
+    
+    plt.rc('font', size=12, family='sans-serif', **{'sans-serif': ['Helvetica']})
+    plt.rc('axes', labelsize=12, titlesize=12, titlepad=8, labelpad=6)
+    plt.rc('legend', fontsize=10)
+    plt.rc('xtick', labelsize=8)
+    plt.rc('ytick', labelsize=8)
